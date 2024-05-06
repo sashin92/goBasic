5d3134b504081ed2fff6e4ba927bad77435273d8
Sangbeom Shin <aopaoi0987@gmail.com>
2024-05-06 17:55:40 +0900

    update hitURL

--- hitURL/main.go
+++ hitURL/main.go
@@ -1,61 +1,46 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"time"
+	"net/http"
 )
 
-var (
-	errRequestFailed = errors.New("Request Failed")
-)
+type requestResult struct {
+	url    string
+	status string
+}
 
 func main() {
-	go coolCount("sashin")
-	go coolCount("nico")
-	time.Sleep(time.Second * 5)
-}
-
-func coolCount(preson string) {
-	for i := 0; i < 10; i++ {
-		fmt.Println(preson, "is cool!", i)
-		time.Sleep(time.Second)
+	results := make(map[string]string)
+	c := make(chan requestResult)
+	urls := []string{
+		"https://www.airbnb.com",
+		"https://www.google.com",
+		"https://www.amazon.com",
+		"https://www.reddit.com",
+		"https://www.google.com",
+		"https://soundcloud.com",
+		"https://www.facebook.com",
+		"https://www.instagram.com",
 	}
+	for _, url := range urls {
+		go hitURL(url, c)
+	}
+	for i := 0; i < len(urls); i++ {
+		result := <-c
+		results[result.url] = result.status
+	}
+	for url, status := range results {
+		fmt.Println(url, "status?", status)
+	}
+
 }
 
-// func main() {
-
-// 	urls := []string{
-// 		"https://www.airbnb.com",
-// 		"https://www.google.com",
-// 		"https://www.amazon.com",
-// 		"https://www.reddit.com",
-// 		"https://www.google.com",
-// 		"https://soundcloud.com",
-// 		"https://www.facebook.com",
-// 		"https://www.instagram.com",
-// 	}
-
-// 	var results = make(map[string]string)
-// 	for _, url := range urls {
-// 		result := "OK"
-// 		err := hitURL(url)
-// 		if err != nil {
-// 			result = "FAILED"
-// 		}
-// 		results[url] = result
-// 	}
-// 	for url, result := range results {
-// 		fmt.Println(url, result)
-// 	}
-// }
-
-// func hitURL(url string) error {
-// 	fmt.Println("Checking: ", url)
-// 	resp, err := http.Get(url)
-// 	if err != nil || resp.StatusCode >= 400 {
-// 		fmt.Println(err)
-// 		return errRequestFailed
-// 	}
-// 	return nil
-// }
+func hitURL(url string, c chan<- requestResult) {
+	resp, err := http.Get(url)
+	status := "OK"
+	if err != nil || resp.StatusCode >= 400 {
+		status = "FAILED"
+	}
+	c <- requestResult{url: url, status: status}
+}
